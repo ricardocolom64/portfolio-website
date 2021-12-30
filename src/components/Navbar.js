5fd4cb79855bfdfe9b7d3dc623768246322263cd
ricardocolom64 <ricardocolom64@gmail.com>
2021-12-30 01:26:03 -0500

    Website navbar now scales properly on different screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/Navbar.css";
-import { HashLink } from 'react-router-hash-link';
+import ReorderIcon from "../assets/icons/reorder_white_24dp.svg";
+import { HashLink } from "react-router-hash-link";
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
       <div className="content">
-        <div className="leftSide">
-          <HashLink smooth to="/#home">
-            ricardoColom
-          </HashLink></div>
-
+        <div className="leftSide" id={openLinks ? "open" : "close"}>
+          <HashLink smooth to="/#home" id="name">
+            <b>ricardoColom</b>
+          </HashLink>
+          <div className="hiddenLinks">
+          <HashLink smooth to="/#portfolio">
+            portfolio
+          </HashLink>
+          <HashLink smooth to="/#projects">
+            projects
+          </HashLink>
+          <HashLink smooth to="#contact">
+            contact
+          </HashLink>
+          </div>
+        </div>
         <div className="rightSide">
           <HashLink smooth to="/#portfolio">
             portfolio
@@ -21,6 +39,9 @@ function Navbar() {
           <HashLink smooth to="#contact">
             contact
           </HashLink>
+          <button onClick={toggleNavbar}>
+            <img src={ReorderIcon} />
+          </button>
         </div>
       </div>
     </div>
